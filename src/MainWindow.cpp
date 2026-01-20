d3ee738b3a36df6d29d46770be6f2abb0ddaf96b
云间 <inku2333@users.noreply.github.com>
2026-01-20 11:49:03 +0800

    Update MainWindow.cpp

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -35,4 +35,40 @@ void MainWindow::initUI() {
     m_navBar->setFixedWidth(240);
     mainLayout->addWidget(m_navBar);
 
-    m_contentStack = new QStackedWidget(thi
+    m_contentStack = new QStackedWidget(this);
+    mainLayout->addWidget(m_contentStack);
+
+    setupSidebar();
+    setupContent();
+
+    auto statusBar = this->statusBar();
+    statusBar->showMessage(tr("System Initialized (Qt6)"));
+
+    connect(m_navBar, &QListWidget::currentRowChanged,
+            m_contentStack, &QStackedWidget::setCurrentIndex);
+
+    if(m_navBar->count() > 0) m_navBar->setCurrentRow(0);
+}
+
+void MainWindow::setupSidebar() {
+    auto add = [&](const QString& text, QStyle::StandardPixmap icon) {
+        auto item = new QListWidgetItem(this->style()->standardIcon(icon), text);
+        m_navBar->addItem(item);
+    };
+
+    add(tr("Device Manager"), QStyle::SP_ComputerIcon);
+    add(tr("Configuration"), QStyle::SP_FileDialogDetailedView);
+    add(tr("Data Acquisition"), QStyle::SP_DriveHDIcon); // <-- 新增
+    add(tr("System Logs"), QStyle::SP_MessageBoxInformation);
+    add(tr("Settings"), QStyle::SP_BrowserReload);
+}
+
+void MainWindow::setupContent() {
+    m_contentStack->addWidget(new DevicePage(this));
+    m_contentStack->addWidget(new ConfigPage(this));
+    m_contentStack->addWidget(new DataPage(this));
+
+    // 占位页面
+    m_contentStack->addWidget(new BasePage("System Logs (Coming Soon)", this));
+    m_contentStack->addWidget(new BasePage("Settings (Coming Soon)", this));
+}
